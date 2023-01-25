a387a6efd7f5d9d882deac0ccea4b5270ff0965d
Ripjeet Singh <singh5455@saskpolytech.ca>
2023-01-25 14:17:17 -0600

    testing generic consumer on hashsets

--- src/ListDemo.java
+++ src/ListDemo.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -7,6 +8,7 @@ public class ListDemo {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("one", "two", "three", "four");
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
 
         list.forEach(new ConsumerImpl<>());
 
@@ -27,6 +29,18 @@ public class ListDemo {
 
         Consumer consumer = t -> System.out.println(t);
         list.forEach(consumer);
+
+        System.out.println("++++++++++++");
+
+        HashSet<Integer> myHash = new HashSet<>();
+        myHash.add(1);
+        myHash.add(2);
+        myHash.add(3);
+        myHash.add(4);
+        myHash.add(5);
+
+        myHash.removeIf(t-> t>2);
+        System.out.println(myHash);
 
     }
 }
