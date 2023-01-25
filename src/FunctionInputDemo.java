204c1fcefe56264aae12247242998ac7efa6d922
Ripjeet Singh <singh5455@saskpolytech.ca>
2023-01-25 14:29:59 -0600

    function input demo

---
+++ src/FunctionInputDemo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionInputDemo {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("one", "two", "three", "four", "five");
+
+        list.forEach(t-> System.out.println(processString(s-> t.toUpperCase(), t)));
+
+    }
+
+    public static String processString(Function<String, String> operation, String target) {
+        return operation.apply(target);
+    }
+}
